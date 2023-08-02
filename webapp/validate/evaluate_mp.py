baa8e2e4502cc1d54378025ab3342b3a892ba8c1
Gerry Fernandez <gerryaf2@gmail.com>
2023-08-02 09:50:11 -0700

    made 7 donor values optional

--- webapp/validate/evaluate_mp.py
+++ webapp/validate/evaluate_mp.py
@@ -353,66 +353,66 @@ def _donor_rule(node: Node) -> list:
                         for stage in stgchild.children:
                             if stage.name not in mdb_names.CYCLE_STAGE:
                                 donornodes[18] = True
-                            if stage.name == mdb_names.FOLLICULAR and (stage.content not in mdb_names.FOLLICULAR_VALUES and stage.attributes.get("value", None) not in mdb_names.FOLLICULAR_VALUES):
-                                donornodes[23] = True
-                            if stage.name == mdb_names.LUTEAL and (stage.content not in mdb_names.LUTEAL_VALUES and stage.attributes.get("value", None) not in mdb_names.LUTEAL_VALUES):
-                                donornodes[24] = True
-                if spcchild.name == mdb_names.DAY_OF_CYCLE:
-                    if spcchild.content:
-                        try:
-                            val = int(spcchild.content)
-                            if val < 0:
-                                raise ValueError
-                            else:
-                                donornodes[28] = True
-                        except ValueError:
-                            pass
-                    else:
-                        donornodes[28] = True   # Node is not required to have content
+                            #if stage.name == mdb_names.FOLLICULAR and (stage.content not in mdb_names.FOLLICULAR_VALUES and stage.attributes.get("value", None) not in mdb_names.FOLLICULAR_VALUES):
+                            #    donornodes[23] = True
+                            #if stage.name == mdb_names.LUTEAL and (stage.content not in mdb_names.LUTEAL_VALUES and stage.attributes.get("value", None) not in mdb_names.LUTEAL_VALUES):
+                            #    donornodes[24] = True
+                #if spcchild.name == mdb_names.DAY_OF_CYCLE:
+                #    if spcchild.content:
+                #        try:
+                #            val = int(spcchild.content)
+                #            if val < 0:
+                #                raise ValueError
+                #            else:
+                #                donornodes[28] = True
+                #        except ValueError:
+                #            pass
+                #    else:
+                #        donornodes[28] = True   # Node is not required to have content
         if child.name == mdb_names.SPEC_LOCATION:
             for slchild in child.children:
                 if slchild.name == mdb_names.CORPUS_LUTEUM and (slchild.content not in mdb_names.CORPUS_LUTEUM_VALUES and slchild.attributes.get("value", None) not in mdb_names.CORPUS_LUTEUM_VALUES):
                     donornodes[22] = True
                 if slchild.name not in mdb_names.SPEC_LOCATION_VALUES:
                     donornodes[30] = True
-        if child.name == mdb_names.DONOR_AGE:
-            for achild in child.children:
-                if achild.name == mdb_names.DONOR_YEARS:
-                    if achild.content:
-                        try:
-                            val = int(achild.content)
-                            if val < 0:
-                                raise ValueError
-                            else:
-                                donornodes[26] = True
-                        except ValueError:
-                            pass
-                    else:
-                        donornodes[26] = True   # Node is not required to have content
-                if achild.name == mdb_names.DONOR_DAYS:
-                    if achild.content:
-                        try:
-                            val = int(achild.content)
-                            if val < 0:
-                                raise ValueError
-                            else:
-                                donornodes[27] = True
-                        except ValueError:
-                            pass
-                    else:
-                        donornodes[27] = True  # Node is not required to have content
-        if child.name == mdb_names.SEC_SEQ_NUM:
-            if child.content:
-                try:
-                    val = int(child.content)
-                    if val < 0:
-                        raise ValueError
-                    else:
-                        donornodes[29] = True
-                except ValueError:
-                    pass
-            else:
-                donornodes[29] = True  # Node is not required to have content
+        #if child.name == mdb_names.DONOR_AGE:
+        #    for achild in child.children:
+        #        if achild.name == mdb_names.DONOR_YEARS:
+        #            if achild.content:
+        #                try:
+        #                    val = int(achild.content)
+        #                    if val < 0:
+        #                        raise ValueError
+        #                    else:
+        #                        donornodes[26] = True
+        #                except ValueError:
+        #                    pass
+        #            else:
+        #                donornodes[26] = True   # Node is not required to have content
+        #        if achild.name == mdb_names.DONOR_DAYS:
+        #            if achild.content:
+        #                try:
+        #                    val = int(achild.content)
+        #                    if val < 0:
+        #                        raise ValueError
+        #                    else:
+        #                        donornodes[27] = True
+        #                except ValueError:
+        #                    pass
+        #            else:
+        #                donornodes[27] = True  # Node is not required to have content
+        #if child.name == mdb_names.SEC_SEQ_NUM:
+        #    if child.content:
+        #        try:
+        #            val = int(child.content)
+        #            if val < 0:
+        #                raise ValueError
+        #            else:
+        #                donornodes[29] = True
+        #        except ValueError:
+        #            pass
+        #    else:
+        #        donornodes[29] = True  # Node is not required to have content
 
 
         # if child.name == mdb_names.MICROSCOPE:
@@ -554,54 +554,54 @@ def _donor_rule(node: Node) -> list:
             f'Donor Ovary Position must be a valid position.',
             node
         ))
-    if donornodes[22]:
-        evaluation.append((
-            EvaluationWarningMp.DONOR_CORPUS_LUTEUM_ENUM,
-            f'Donor Corpus Luteum Type must be a valid type.',
-            node
-        ))
-    if donornodes[23]:
-        evaluation.append((
-            EvaluationWarningMp.DONOR_FOLLICULAR_ENUM,
-            f'Donor Follicular Values must be a valid type.',
-            node
-        ))
-    if donornodes[24]:
-        evaluation.append((
-            EvaluationWarningMp.DONOR_LUTEAL_ENUM,
-            f'Donor Luteal Values must be a valid type.',
-            node
-        ))
+    # if donornodes[22]:
+    #     evaluation.append((
+    #         EvaluationWarningMp.DONOR_CORPUS_LUTEUM_ENUM,
+    #         f'Donor Corpus Luteum Type must be a valid type.',
+    #         node
+    #     ))
+    # if donornodes[23]:
+    #     evaluation.append((
+    #         EvaluationWarningMp.DONOR_FOLLICULAR_ENUM,
+    #         f'Donor Follicular Values must be a valid type.',
+    #         node
+    #     ))
+    # if donornodes[24]:
+    #     evaluation.append((
+    #         EvaluationWarningMp.DONOR_LUTEAL_ENUM,
+    #         f'Donor Luteal Values must be a valid type.',
+    #         node
+    #     ))
     if donornodes[25]:
         evaluation.append((
             EvaluationWarningMp.DONOR_SUDAN_STAIN_ENUM,
             f'Donor Sudan Stain Value must be a valid value.',
             node
         ))
-    if not donornodes[26]:
-        evaluation.append((
-            EvaluationWarningMp.DONOR_YEARS_NON_NEGATIVE,
-            f'Donor Years must be a non-negative value.',
-            node
-        ))
-    if not donornodes[27]:
-        evaluation.append((
-            EvaluationWarningMp.DONOR_DAYS_NON_NEGATIVE,
-            f'Donor Days must be a non-negative value.',
-            node
-        ))
-    if not donornodes[28]:
-        evaluation.append((
-            EvaluationWarningMp.DONOR_DAY_OF_CYCLE_NON_NEGATIVE,
-            f'Donor Day Of Cycle must be a non-negative value.',
-            node
-        ))
-    if not donornodes[29]:
-        evaluation.append((
-            EvaluationWarningMp.DONOR_SEC_SEQ_NUM_NON_NEGATIVE,
-            f'Donor Section Sequence Number must be a non-negative value.',
-            node
-        ))
+    # if not donornodes[26]:
+    #     evaluation.append((
+    #         EvaluationWarningMp.DONOR_YEARS_NON_NEGATIVE,
+    #         f'Donor Years must be a non-negative value.',
+    #         node
+    #     ))
+    # if not donornodes[27]:
+    #     evaluation.append((
+    #         EvaluationWarningMp.DONOR_DAYS_NON_NEGATIVE,
+    #         f'Donor Days must be a non-negative value.',
+    #         node
+    #     ))
+    # if not donornodes[28]:
+    #     evaluation.append((
+    #         EvaluationWarningMp.DONOR_DAY_OF_CYCLE_NON_NEGATIVE,
+    #         f'Donor Day Of Cycle must be a non-negative value.',
+    #         node
+    #     ))
+    # if not donornodes[29]:
+    #     evaluation.append((
+    #         EvaluationWarningMp.DONOR_SEC_SEQ_NUM_NON_NEGATIVE,
+    #         f'Donor Section Sequence Number must be a non-negative value.',
+    #         node
+    #     ))
     if donornodes[30]:
         evaluation.append((
             EvaluationWarningMp.DONOR_SPEC_LOCATION_CHOICE,
